adff6012e80b313e5b4cad1ebe2c3e0371cf4407
kkatoch89 <karankatoch.hba@gmail.com>
2020-10-02 16:21:02 -0400

    Started pseudo-code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ class App extends Component {
   }
   render() {
     return (
-      
+      // HEADER
+      // Parts: h1, smoothscroll btn, tagline
+      // SEARCH-PAGE
+      // Parts: h2, Form(Searchbar, submit btn), p(instructions)
+      // State: Array of gif objects
+      // RESULTS-PAGE
+      // Parts: h2, Results/gifs, Regenerate btn with p description
+      // Firebase setup (each entry will be an obj containing user selections WITH present date) 
+      // HISTORY/TIMELINE
+      // Parts: h2, Component from react vertical timeline, gifs mathcing date that populate timeline
+      // 
+
     );
   }
 }
