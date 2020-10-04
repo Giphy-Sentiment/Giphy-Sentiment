58fef696c9a65daf9adb04cf4d35411befbf5428
Your <yourname@your-email.com>
2020-10-04 10:07:13 -0400

    fixed the error

--- src/Components/SearchNResults.js
+++ src/Components/SearchNResults.js
@@ -5,11 +5,14 @@ import Results from './Results';
 class SearchNResults extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '', submitInput: '', gifsArray: [], offset: 0, toSlice: []};
-
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-    console.log(`line 13`, props);
+    this.state = { 
+      value: '', 
+      submitInput: '', 
+      gifsArray: [], 
+      offset: 0, 
+      gifsUrlArr: [],
+      toSlice: [],
+    };
   }
   getGif = async (userInput) => {
     const key = 'e6I6PjSAevodOVfP9kWE6ivjPXnDObA6';
@@ -21,22 +24,26 @@ class SearchNResults extends React.Component {
             `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${searchPhrase}&limit=${limit}`
         )
         .then((res) => {
-            console.log(`data obj`, res);
             const gifsArr = res.data.data;
-            let gifsUrlArr = [];
-            
-            const toSlice = this.state.gifsArray.slice(0, 5);
-            console.log(toSlice);
-            
-            gifsArr.forEach((gifObj) => {
-                gifsUrlArr.push(gifObj.images.fixed_height.url);
-            });
-            
             this.setState({
-                gifsArray: gifsUrlArr, 
-                toSlice: toSlice,
+              gifsArray: gifsArr,
             });
 
+            const gifsUrlArr = [];
+
+             gifsArr.forEach((gifObj) => {
+              gifsUrlArr.push(gifObj.images.fixed_height.url);
+            });
+          
+            this.setState({
+              gifsUrlArr
+            })
+            
+            const toSlice = gifsUrlArr.slice(0, 5);
+
+            this.setState({
+              toSlice
+            })
         });
 };
 
@@ -45,35 +52,30 @@ class SearchNResults extends React.Component {
   }
 
   handleSubmit(event) {
-    console.log('handle submit');
     event.preventDefault();
     const input = this.state.value;
-    // this set state isnt working, check later if time
     this.getGif(input);    
     this.setState({ submitInput: input });
-    // console.log(input);
   }
 
   handleRegenerate(e) {
     e.preventDefault();
-    console.log(`data from regenerate`, this.state.offset);
     let thing = this.state.offset + 5
-
-    const arrSlice = this.state.gifsArray.slice(thing, 5 + thing);
-    
-    this.setState({ offset: thing, toSlice: arrSlice });
+    const arrSlice = this.state.gifsUrlArr.slice(thing, 5 + thing);
+    this.setState({ 
+      offset: thing, 
+      toSlice: arrSlice 
+    });
   }
 
   render() {
-    // console.log('this.state.gifs');
     return (
     <>
       <form onSubmit= {(e) => this.handleSubmit(e)}>
         <label htmlFor="searchterm"><p>Search Gallery:</p></label>
         
           <input type="text" name="searchterm" id="searchterm" value={this.state.value} onChange={(e) => this.handleChange(e)} required />
-          <button type="submit" >Submit!</button>
-          {/* <input type="submit" className="fas fa-search" aria-label="search" value="&#xf002;" /> */}
+          <button type="submit">Submit!</button>
         
       </form>
         <button onClick={(e) => this.handleRegenerate(e)} >Regenerate</button>
@@ -85,6 +87,3 @@ class SearchNResults extends React.Component {
   }
 }
 export default SearchNResults;
-
-
-// if (this.props.onSubmit && typeof this.props.onSubmit === "function")
