a4197e5ae124a0fa84364212e721d9d59c56dcd2
Your <yourname@your-email.com>
2020-10-04 17:27:16 -0400

    fixed display error with firebase, timeline comp

--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
+import firebase from '../firebase';
+ 
+class Timeline extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firebaseData: []
+    }
+  }
 
-const Timeline = (props) => {
-	return (
-		<ul>{/* {props.fbData.map((obj) => {
-				return console.log(obj);
-			})} */}</ul>
-	);
-};
+  componentDidMount() {
+		const dbRef = firebase.database().ref();
+		dbRef.on('value', (res) => {
+			const newState = [];
+			const data = res.val();
+			for (let key in data) {
+				newState.push(data[key]);
+			}
+			this.setState({
+				firebaseData: newState,
+			});
+		});
+  }
+
+  render() {
+    return (
+      <div className='timeline'>
+        {this.state.firebaseData.map((gifObj) => {
+          return (
+            <div>
+              <time>{gifObj.date}</time>
+              <h2>{gifObj.word}</h2>
+              <img src={gifObj.url} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+} 
+
 
 export default Timeline;
