917c268ea0cffa7101e8d735c9f142f46bd63318
Ismail Khan <ismailislamkhan@gmail.com>
2020-10-06 17:32:57 -0400

    Updates

---
+++ precache-manifest.8bd2d61c270f3114f0018c0756246421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e97e8fe4323b512a353c18dd3943d0",
+    "url": "/project-6/index.html"
+  },
+  {
+    "revision": "a4c088a2b3b00474b969",
+    "url": "/project-6/static/css/2.ef8b4205.chunk.css"
+  },
+  {
+    "revision": "61a7b5ba82040b31d52c",
+    "url": "/project-6/static/css/main.bb9057c7.chunk.css"
+  },
+  {
+    "revision": "a4c088a2b3b00474b969",
+    "url": "/project-6/static/js/2.6133c3bc.chunk.js"
+  },
+  {
+    "revision": "7e63ba2e8ab1f33082bd7852239989e8",
+    "url": "/project-6/static/js/2.6133c3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a7b5ba82040b31d52c",
+    "url": "/project-6/static/js/main.7a577867.chunk.js"
+  },
+  {
+    "revision": "f279b6017d39481ac1a2",
+    "url": "/project-6/static/js/runtime-main.6a52efb2.js"
+  }
+]);
